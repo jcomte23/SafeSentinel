01be5f1eea3c19954b42e837f61647a164ab4cf1
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-10 23:00:59 -0500

    mejora en la barra de navegacion

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link href="{{ route('settings') }}" :active="request()->routeIs('settings','types_of_documents.*')">
+                    <x-nav-link href="{{ route('settings') }}" :active="request()->routeIs('settings','types_of_documents.*','international_phone_codes.*')">
                         {{ __('Settings') }}
                     </x-nav-link>
                     <x-nav-link href="{{ route('employees.index') }}" :active="request()->routeIs('employees.*')">
