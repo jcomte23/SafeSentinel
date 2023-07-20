0a6f4f2b37be9b0f97433457e62f0a872ac44d60
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-19 23:57:40 -0500

    creacion de tabla expediente empleado

---
+++ app/Models/FileEmployee.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FileEmployee extends Model
+{
+    use HasFactory;
+}
